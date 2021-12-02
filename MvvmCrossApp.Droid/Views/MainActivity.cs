614873331b34adc16823bf9179e76b5c7642ecc0
FranPeraba <francisco.peraba@gmail.com>
2021-12-02 20:11:13 +0100

    Droid Progress bar implementation

--- MvvmCrossApp.Droid/Views/MainActivity.cs
+++ MvvmCrossApp.Droid/Views/MainActivity.cs
@@ -1,6 +1,8 @@
-﻿using Android.App;
+﻿using System.ComponentModel;
+using Android.App;
 using Android.OS;
-using AndroidX.AppCompat.Widget;
+using Android.Views;
+using Android.Widget;
 using AndroidX.RecyclerView.Widget;
 using MvvmCross.DroidX.RecyclerView;
 using MvvmCross.Platforms.Android.Binding.BindingContext;
@@ -9,6 +11,7 @@ using MvvmCross.Platforms.Android.Views;
 using MvvmCrossApp.Core.Resources;
 using MvvmCrossApp.Core.ViewModels;
 using MvvmCross.Binding.BindingContext;
+using SearchView = AndroidX.AppCompat.Widget.SearchView;
 
 namespace MvvmCrossApp.Droid.Views
 {
@@ -19,17 +22,27 @@ namespace MvvmCrossApp.Droid.Views
         MvxRecyclerView _recyclerView;
         MedicinesAdapter _adapter;
         SearchView _searchView;
+        ProgressBar _progressBar;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
+            _progressBar = FindViewById<ProgressBar>(Resource.Id.progress_bar);
+            _progressBar.Visibility = ViewStates.Gone;
+
             SetupSearchView();
 
             SetupRecyclerView();
 
             SetupBindings();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            ClearBindings();
         }
 
         void SetupSearchView()
@@ -59,6 +72,23 @@ namespace MvvmCrossApp.Droid.Views
             var bindingSet = this.CreateBindingSet<MainActivity, SearchMainViewModel>();
             bindingSet.Bind(_adapter).For(v => v.ItemsSource).To(vm => vm.Medicines);
             bindingSet.Apply();
+            
+            ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+
+        void ClearBindings()
+        {
+            ViewModel.PropertyChanged -= ViewModel_PropertyChanged;
+        }
+
+        void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(ViewModel.IsLoading):
+                    _progressBar.Visibility = !ViewModel.IsLoading ? ViewStates.Gone : ViewStates.Visible;
+                    break;
+            }
         }
 
         public bool OnQueryTextChange(string newText)
