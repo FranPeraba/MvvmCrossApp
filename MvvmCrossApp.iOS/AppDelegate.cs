832a1d58a8ea5c3931759b337f0d2bc2a3b0b0a1
FranPeraba <francisco.peraba@gmail.com>
2021-12-08 11:06:10 +0100

    Add iOS project

---
+++ MvvmCrossApp.iOS/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using MvvmCrossApp.Core;
+using UIKit;
+
+namespace MvvmCrossApp.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MvxApplicationDelegate<Setup, App>
+    {
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            var result = base.FinishedLaunching(application, launchOptions);
+
+            return result;
+        }
+
+    }
+}
+
---
+++ MvvmCrossApp.iOS/Setup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MvvmCross.Platforms.Ios.Core;
+using MvvmCrossApp.Core;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace MvvmCrossApp.iOS
+{
+    public class Setup : MvxIosSetup<App>
+    {
+        protected override ILoggerFactory CreateLogFactory()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.NSLog()
+                .CreateLogger();
+
+            return new SerilogLoggerFactory();
+        }
+
+        protected override ILoggerProvider CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+    }
+}
