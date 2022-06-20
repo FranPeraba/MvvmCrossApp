c4eeaebe71597a350152c4ce2c03c2492ff511b9
Francisco José Perabá <francisco.peraba@gmail.com>
2022-06-20 12:17:30 +0200

    iOS Refactor SearchView

---
+++ MvvmCrossApp.iOS/Sources/MedicinesTableViewSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Foundation;
+using MvvmCross.Platforms.Ios.Binding.Views;
+using MvvmCrossApp.iOS.Views;
+using UIKit;
+
+namespace MvvmCrossApp.iOS.Sources
+{
+	public class MedicinesTableViewSource: MvxTableViewSource
+	{
+		public MedicinesTableViewSource(UITableView tableView): base(tableView)
+		{
+			DeselectAutomatically = true;
+		}
+
+        protected override UITableViewCell GetOrCreateCellFor(UITableView tableView, NSIndexPath indexPath, object item)
+        {
+			var cell = tableView.DequeueReusableCell("medicineCell", indexPath) as MedicineCell;
+
+			return cell;
+        }
+    }
+}
+
