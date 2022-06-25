b5665211915dbb5384496066c606d34a4b508b13
Francisco José Perabá <francisco.peraba@gmail.com>
2022-06-25 20:26:17 +0200

    FORMS UI Add Forms.UI project

---
+++ MvvmCrossApp.Forms.UI/FormsApp.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MvvmCrossApp.Forms.UI
+{
+    public partial class FormsApp : Application
+    {
+        public FormsApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
