780444beb8207b656a175e60d7e2a7da79fc1aa3
Francisco José Perabá <francisco.peraba@gmail.com>
2022-06-26 10:54:51 +0200

    FORMS DROID Add Android project

---
+++ MvvmCrossApp.Forms.Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using MvvmCross.Forms.Platforms.Android.Views;
+using MvvmCrossApp.Core;
+using MvvmCrossApp.Forms.UI;
+
+namespace MvvmCrossApp.Forms.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        LaunchMode = LaunchMode.SingleTask)]
+    public class MainActivity : MvxFormsAppCompatActivity<Setup, App, FormsApp>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            
+            base.OnCreate(bundle);
+        }
+    }
+}
---
+++ MvvmCrossApp.Forms.Droid/Setup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCrossApp.Core;
+using MvvmCrossApp.Forms.UI;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace MvvmCrossApp.Forms.Droid
+{
+    public class Setup : MvxFormsAndroidSetup<App, FormsApp>
+    {
+        protected override ILoggerFactory CreateLogFactory()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.AndroidLog()
+                .CreateLogger();
+
+            return new SerilogLoggerFactory();
+        }
+
+        protected override ILoggerProvider CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+    }
+}
+
