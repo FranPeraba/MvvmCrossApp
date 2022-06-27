99f44c0d53967888c5051369545a5633f1eae917
Francisco José Perabá <francisco.peraba@gmail.com>
2022-06-27 13:12:43 +0200

    FORMS iOS Add Forms iOS project

---
+++ MvvmCrossApp.Forms.iOS/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using Foundation;
+using MvvmCross.Forms.Platforms.Ios.Core;
+using MvvmCrossApp.Core;
+using MvvmCrossApp.Forms.UI;
+using UIKit;
+
+namespace MvvmCrossApp.Forms.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register ("AppDelegate")]
+    public class AppDelegate : MvxFormsApplicationDelegate<Setup, App, FormsApp>
+    {
+        public override UIWindow Window { get; set; }
+
+        
+        public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+        {
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
+
+            return base.FinishedLaunching(application, launchOptions);
+        }
+    }
+}
+
+
---
+++ MvvmCrossApp.Forms.iOS/Setup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MvvmCross.Forms.Platforms.Ios.Core;
+using MvvmCrossApp.Core;
+using MvvmCrossApp.Forms.UI;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace MvvmCrossApp.Forms.iOS
+{
+    public class Setup : MvxFormsIosSetup<App, FormsApp>
+    {
+        protected override ILoggerFactory CreateLogFactory()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.NSLog()
+                .CreateLogger();
+
+            return new SerilogLoggerFactory();
+        }
+
+        protected override ILoggerProvider CreateLogProvider()
+        {
+            return new SerilogLoggerProvider();
+        }
+    }
+}
+
